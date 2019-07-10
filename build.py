146c63b8ff03b84b3172e29199c573fd3880397f
Matthew Knight <mgk1795@mail.com>
2019-07-09 18:52:15 -0700

    updated build.py

--- build.py
+++ build.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
 
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder()
-    builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
+    builder.add({"os" : build_shared.get_os()}, {}, {}, {})
     builder.run()
